8548d3bc8fdf391140ad29dc891e98e35314f728
E. K. Lee <wolf@teoal.net>
2025-12-04 16:52:40 +0900

    3장 Taco 저장까지 실습

--- ch03/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ ch03/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -8,11 +8,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import tacos.Ingredient;
+import tacos.Order;
 import tacos.Ingredient.Type;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 import tacos.Taco;
 
 import java.util.ArrayList;
@@ -22,28 +27,19 @@ import java.util.stream.Collectors;
 @Slf4j // SLF4J 로거 생성
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
     private final IngredientRepository ingredientRepository;
+    private TacoRepository tacoRepository;
     
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepository) {
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.tacoRepository = tacoRepository;
     }
 
     @GetMapping
     public String showDesignForm(Model model) {
-        // List<Ingredient> ingredients = Arrays.asList(
-        //         new Ingredient("FLTO", "흰 밀가루 또띠아", Type.WRAP),
-        //         new Ingredient("COTO", "옥수수 또띠아", Type.WRAP),
-        //         new Ingredient("GRBF", "갈은 소고기", Type.PROTEIN),
-        //         new Ingredient("CARN", "돼지고기 속", Type.PROTEIN),
-        //         new Ingredient("TMTO", "다진 토마토", Type.VEGGIES),
-        //         new Ingredient("LETC", "양상추", Type.VEGGIES),
-        //         new Ingredient("CHED", "체다 치즈", Type.CHEESE),
-        //         new Ingredient("JACK", "몬테레이 잭 치즈", Type.CHEESE),
-        //         new Ingredient("SLSA", "살사 소스", Type.SAUCE),
-        //         new Ingredient("SRCR", "사워 크림", Type.SAUCE)
-        // );
 
         // 근데 바로 리스트 안 만들고 왜 이렇게 할까?
         List<Ingredient> ingredients = new ArrayList<>();
@@ -59,13 +55,24 @@ public class DesignTacoController {
         return "design";
     }
 
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             return "design";
         }
-    //    여기서 타코 디자인을 저장...
-        log.info("Processing design: " + design);
+        Taco saved = tacoRepository.save(design);
+        order.addDesign(saved);
+        
         return "redirect:/orders/current";
     }
 
